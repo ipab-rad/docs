ebb8befece019faea47afd861c924961e98badac
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-19 14:12:06 +0000

    Add arp_fix.sh and edit dbstartup

---
+++ youbot_common/arp_fix.sh
@@ -0,0 +1,8 @@
+# soundwave
+sudo arp -s 192.168.106.9 c0:4a:00:1c:3e:5e
+# megatron
+arp -s 192.168.106.7 c0:4a:00:1c:2e:a7
+# starcream
+arp -s 192.168.106.11 c0:4a:00:1a:d6:cc
+# blackout
+arp -s 192.168.106.13 c0:4a:00:1e:96:fa
