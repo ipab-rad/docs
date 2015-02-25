cc195f1fe4569679c5381f0075bdbfe7ff127e03
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-25 17:15:38 +0000

    Remove arp lines from /etc/ethers

--- youbot_common/arp_fix.sh
+++ youbot_common/arp_fix.sh
@@ -1,10 +1,25 @@
-# Copy this in /etc/ethers
-
+##### YOUBOTS
 # soundwave
-sudo arp -s 192.168.106.9 c0:4a:00:1c:3e:5e
+c0:4a:00:1c:3e:5e 192.168.106.9
 # megatron
-arp -s 192.168.106.7 c0:4a:00:1c:2e:a7
+c0:4a:00:1c:2e:a7 192.168.106.7
 # starcream
-arp -s 192.168.106.11 c0:4a:00:1a:d6:cc
+c0:4a:00:1a:d6:cc 192.168.106.11
 # blackout
-arp -s 192.168.106.13 c0:4a:00:1e:96:fa
+c0:4a:00:1e:96:fa 192.168.106.13
+##### PCs
+# cosmos
+94:de:80:e3:2a:35 192.168.106.18
+# windcharger
+54:a0:50:85:31:d8 192.168.106.14
+# tailgate
+18:67:b0:bd:e9:75 192.168.106.19
+# strafe
+94:de:80:e3:9f:e5 192.168.106.21
+# beachcomber
+78:2b:cb:8c:ee:dc 192.168.106.22
+# determinist
+28:d2:44:0f:33:f8 192.168.106.16
+6c:88:14:84:90:04 192.168.106.17
+# jazz / b8:ca:3a:b5:d2:e7
+# blurr / b8:ca:3a:7f:fa:2d
